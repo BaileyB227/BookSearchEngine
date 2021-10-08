ba491f8e022a2ff909fcb395b6a089f6e9f50343
BaileyB227 <87950291+BaileyB227@users.noreply.github.com>
2021-10-08 09:51:10 -0500

    compiling issue

--- server/server.js
+++ server/server.js
@@ -6,8 +6,6 @@ const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers} = require('./schemas');
 const {authMiddleware} = require('./utils/auth');
 
-const db = require('./config/connection');
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
