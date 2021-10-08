0d930d3c0b08c3479e85bc6bbc72ab3c050dbfab
BaileyB227 <87950291+BaileyB227@users.noreply.github.com>
2021-10-08 10:39:23 -0500

    fixed serverjs

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+const mongoose = require("mongoose");
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
-const { ApolloServer } = require('apollo-server-express');
-const { typeDefs, resolvers} = require('./schemas');
-const {authMiddleware} = require('./utils/auth');
+
+const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,28 +15,39 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware
-});
-
-server.applyMiddleware({ app });
-
-await apolloServer.start();
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+})
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/booksearch", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
+server.applyMiddleware({app});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+
 
 app.use(routes);
 
+
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+
+  });
+});
+
+process.on('uncaughtException', function(err) {
+  console.log('Caught exception: ' + err);
 });
